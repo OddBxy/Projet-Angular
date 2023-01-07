14a1fada0323470af3ae7178dbc3cbf2ffa5d73b
OddBxy <118909861+OddBxy@users.noreply.github.com>
2023-01-07 19:25:50 +0100

    Optimisation de la pt 2 pour faciliter la futur pt3
    Le but est de créer une liste de listes (appelé "listes") dans app.component.ts avec un model dans app.model.taches
    Ainsi au lieu d'avoir 4 div pour chaque liste dans l'html, on en créer une seule avec l'attribut *ngFor="let liste of listes".
    Ainsi dans la partie 3, lorsqu'on créer une nouvelle liste, celle-ci sera ajoutée à "listes" et une nouvelle div sera ajouté automatiquement dans notre page

---
+++ PARTIE-ANGULAR/src/app/component/taches/taches.component.ts
@@ -0,0 +1,135 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Liste, Tache } from 'src/app/model/tache';
+import { TachesService } from 'src/app/service/taches.service';
+import { UserService } from 'src/app/service/user.service';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { json } from 'express';
+
+@Component({
+  selector: 'app-taches',
+  templateUrl: './taches.component.html',
+  styleUrls: ['./taches.component.css']
+})
+export class TachesComponent implements OnInit {
+  taches: Array<Tache> = [];
+  newTache: Tache = {
+    titre : '',
+    termine : false,
+    statut: ''
+  };  
+
+  UNDIFINED: Liste = {
+    titre: 'UNDEFINED',
+    statut:'',
+    Taches: this.taches
+  }
+
+  ATTENTE: Liste = {
+    titre: 'ATTENTE',
+    statut: 'ATTENTE',
+    Taches: this.taches
+  }
+
+  COURANT: Liste = {
+    titre: 'EN COURS',
+    statut: 'EN COURS',
+    Taches: this.taches
+  }
+
+  TERMINE: Liste = {
+    titre: 'TERMINE',
+    statut: 'TERMINE',
+    Taches: this.taches
+  }
+
+  listes: Array<Liste> = [ this.UNDIFINED, this.ATTENTE, this.COURANT, this.TERMINE]
+
+  filter:string = 'Tous';
+
+  constructor(private tacheService: TachesService,
+    private userService: UserService,
+    private router: Router){ }
+  
+  ngOnInit(): void {
+    this.tacheService.getTaches().subscribe({
+      next: (data:Array<Tache>) => { this.taches = data; }
+    });
+
+  }  
+  ajouter(param:string) {
+    this.newTache.statut =  param;
+    this.tacheService.ajoutTaches(this.newTache).subscribe({
+      next: (data) => {
+        this.taches.push(data);
+      }
+    });
+    
+  }  
+
+  supprimer(tache: Tache): void {
+    this.tacheService.removeTaches(tache).subscribe({
+      next: (data) => {
+        this.taches = this.taches.filter(t => tache._id != t._id);
+      }
+    });
+
+  }
+
+  modifier(tache: Tache) {
+    tache.termine = !tache.termine;
+    this.tacheService.updateTaches(tache).subscribe({
+      next: (data) => {
+      }
+    });
+  }
+
+  loggout() {
+    this.userService.logout().subscribe(() => {
+      this.router.navigate(['']);
+    })
+  }
+
+  change(filter:string) {
+    this.filter = filter;
+  }
+
+  drop(event: CdkDragDrop<string[]>, statut:string) {
+    
+    var tache: Tache = {
+      _id: event.item.data._id,
+      titre: event.item.data.titre,
+      termine: false,
+      statut:''
+    }
+
+    
+    if(statut == "UNDIFINED"){
+      event.item.data.statut = ''  
+      tache.statut = ''
+      this.tacheService.updateTaches(tache).subscribe({
+        next: (data) => {
+        }
+      });
+    }
+    else{
+      event.item.data.statut = statut
+      tache.statut = statut
+      this.tacheService.updateTaches(tache).subscribe({
+        next: (data) => {
+        }
+      });
+    }
+    
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+    
+  }
+}
