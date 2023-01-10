1553e5a8680e718bceedf58c0bb1873b7ca370bb
OddBxy <118909861+OddBxy@users.noreply.github.com>
2023-01-10 20:42:54 +0100

    Delete Partie-API-rest/site directory

--- Partie-API-rest/site/browser.js
+++
@@ -1,8 +0,0 @@
-
-import { getTaches } from "./api.js";
-import { Application } from "./app.js";
-
-// récupère la liste des tâches à partir du webService de l'exercice1
-const taches = await getTaches();
-
-let app = new Application(taches);
