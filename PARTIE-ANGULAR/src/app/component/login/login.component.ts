62f481856274ba4255c5e39d40e845924c43e21e
OddBxy <118909861+OddBxy@users.noreply.github.com>
2023-01-16 21:19:34 +0100

    Add files via upload

---
+++ PARTIE-ANGULAR/src/app/component/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/service/user.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  error: boolean = false;
+  user: User = { login: '', password: '' };
+
+  constructor(private userService: UserService, private router: Router) {
+
+  }
+
+  submit():void {
+      this.userService.login(this.user).subscribe({
+        next: () => { this.router.navigate(['taches']) },
+        error: () => { this.error = true; }
+      });
+  }
+
+  signIn(){
+    console.log(this.user.login, this.user.password)
+    this.userService.signIn(this.user).subscribe({
+      next: () =>{ this.submit() }
+    });
+  }
+}
---
+++ PARTIE-ANGULAR/src/app/model/liste.ts
@@ -0,0 +1,7 @@
+import { Tache } from "./tache";
+export interface Liste{
+    _id?:string
+    titre:string;
+    statut:string
+    Taches:Array<Tache>;
+}
